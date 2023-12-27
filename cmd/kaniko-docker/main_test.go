e6884223ed700e0f4a9c5b9d7e95560aa1c675cd
going <going.miao@xianghuanji.com>
2023-12-27 21:25:30 +0800

    适配易奢堂发布逻辑

--- cmd/kaniko-docker/main_test.go
+++ cmd/kaniko-docker/main_test.go
@@ -35,3 +35,93 @@ func Test_buildRepo(t *testing.T) {
 		})
 	}
 }
+
+func TestFormatRepoName(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "Example test case 1",
+			input: "MyRepo",
+			want:  "myrepo",
+		},
+		{
+			name:  "Example test case 2",
+			input: "my-repo",
+			want:  "my-repo",
+		},
+		{
+			name:  "Example test case 3",
+			input: "My.Repo",
+			want:  "my-repo",
+		},
+		{
+			name:  "Example test case 4",
+			input: "my/repo",
+			want:  "my-repo",
+		},
+		{
+			name:  "Example test case 5",
+			input: "saas/service.app",
+			want:  "saas-service-app",
+		},
+		{
+			name:  "Example test case 6",
+			input: "jarvis2/jarvis2.api",
+			want:  "jarvis2-jarvis2-api",
+		},
+		{
+			name:  "Example test case 7",
+			input: "saas/goCrawler",
+			want:  "saas-gocrawler",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := formatRepoName(tt.input); got != tt.want {
+				t.Errorf("formatRepoName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFormatRepoPath(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "Trim repo path",
+			input: "/path/to/repo",
+			want:  "path/to/repo",
+		},
+		{
+			name:  "Trim empty repo path",
+			input: "",
+			want:  "",
+		},
+		{
+			name:  "Trim repo path with trailing slash",
+			input: "xhj-prod-registry-vpc.cn-hangzhou.cr.aliyuncs.com/xhj-image/",
+			want:  "xhj-prod-registry-vpc.cn-hangzhou.cr.aliyuncs.com/xhj-image",
+		},
+		{
+			name:  "Trim repo path with trailing slash",
+			input: "xhj-prod-registry-vpc.cn-hangzhou.cr.aliyuncs.com/xhj-image",
+			want:  "xhj-prod-registry-vpc.cn-hangzhou.cr.aliyuncs.com/xhj-image",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := formatRepoPath(test.input)
+			if got != test.want {
+				t.Errorf("formatRepoPath(%q) = %q, want %q", test.input, got, test.want)
+			}
+		})
+	}
+}
