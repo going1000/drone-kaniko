829ec5eecfb8daa53a7340a1f4a046ceee20a85c
going <going.miao@xianghuanji.com>
2023-12-29 15:26:30 +0800

    1.9.1

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t xhj-prod-registry.cn-hangzhou.cr.aliyuncs.com/image-common/kaniko:1.19.1 .
-docker push xhj-prod-registry.cn-hangzhou.cr.aliyuncs.com/image-common/kaniko:1.19.1
+docker build -t xhj-prod-registry.cn-hangzhou.cr.aliyuncs.com/image-common/kaniko:1.9.1 .
+docker push xhj-prod-registry.cn-hangzhou.cr.aliyuncs.com/image-common/kaniko:1.9.1
